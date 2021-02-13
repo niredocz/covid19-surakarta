f25d4a2774f23197a47e1a3b805599477332dfc2
Daffa Aldzakian Fauzi <daffa.rogers@gmail.com>
2021-02-13 09:50:32 +0700

    add datatables library

--- assets/js/highcharts/highchart.js
+++ assets/js/highcharts/highchart.js
@@ -1,4 +1,4 @@
-Highcharts.chart('highchart', {
+Highcharts.chart('highchart-1', {
 
     chart: {
         scrollablePlotArea: {
@@ -14,11 +14,118 @@ Highcharts.chart('highchart', {
     },
 
     title: {
-        text: 'Daily sessions at www.highcharts.com'
+        text: 'Data Covid-19 Kota Surakarta'
     },
 
     subtitle: {
-        text: 'Source: Google Analytics'
+        text: 'sumber: surakarta.go.id'
+    },
+
+    xAxis: {
+        tickInterval: 7 * 24 * 3600 * 1000, // one week
+        tickWidth: 0,
+        gridLineWidth: 1,
+        labels: {
+            align: 'left',
+            x: 3,
+            y: -3
+        }
+    },
+
+    yAxis: [{ // left y axis
+        title: {
+            text: null
+        },
+        labels: {
+            align: 'left',
+            x: 3,
+            y: 16,
+            format: '{value:.,0f}'
+        },
+        showFirstLabel: false
+    }, { // right y axis
+        linkedTo: 0,
+        gridLineWidth: 0,
+        opposite: true,
+        title: {
+            text: null
+        },
+        labels: {
+            align: 'right',
+            x: -3,
+            y: 16,
+            format: '{value:.,0f}'
+        },
+        showFirstLabel: false
+    }],
+
+    legend: {
+        align: 'left',
+        verticalAlign: 'top',
+        borderWidth: 0
+    },
+
+    tooltip: {
+        shared: true,
+        crosshairs: true
+    },
+
+    plotOptions: {
+        series: {
+            cursor: 'pointer',
+            point: {
+                events: {
+                    click: function (e) {
+                        hs.htmlExpand(null, {
+                            pageOrigin: {
+                                x: e.pageX || e.clientX,
+                                y: e.pageY || e.clientY
+                            },
+                            headingText: this.series.name,
+                            maincontentText: Highcharts.dateFormat('%A, %b %e, %Y', this.x) + ':<br/> ' +
+                                this.y + ' sessions',
+                            width: 200
+                        });
+                    }
+                }
+            },
+            marker: {
+                lineWidth: 1
+            }
+        }
+    },
+
+    series: [{
+        name: 'All sessions',
+        lineWidth: 4,
+        marker: {
+            radius: 4
+        }
+    }, {
+        name: 'New users'
+    }]
+});
+Highcharts.chart('highchart-2', {
+
+    chart: {
+        scrollablePlotArea: {
+            minWidth: 700
+        }
+    },
+
+    data: {
+        csvURL: 'https://cdn.jsdelivr.net/gh/highcharts/highcharts@v7.0.0/samples/data/analytics.csv',
+        beforeParse: function (csv) {
+            return csv.replace(/\n\n/g, '\n');
+        }
+    },
+
+    title: {
+        text: 'Data Covid-19 Kota Surakarta'
+    },
+
+    subtitle: {
+        text: 'sumber: surakarta.go.id'
     },
 
     xAxis: {
